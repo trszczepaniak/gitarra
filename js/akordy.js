bf12e617b9cfccfff0d47bbbf940a9cf53873670
Tomasz Szczepaniak <tomasz.szczepaniak@cgmpolska.pl>
2016-11-21 15:54:56 +0100

    tabela dzwięków

--- js/akordy.js
+++ js/akordy.js
@@ -1,14 +1,26 @@
 const gama = ["c", "cis", "d", "dis", "e", "f", "fis", "g", "gis", "a", "b", "h"];
 
-const xdur = [[0,4,7],'dur'];
-const xmol = [[0,3,7],'mol'];
-const xzm = [[0,3,6],'zmniejszone'];
-const xzw = [[0,4,8],'zwiekszone'];
-const dur7 = [[0,4,7,10],'dur septymowy (7)'];
-const mol7 = [[0,3,8,10],'mol septymowy (7)'];
-const sus2 = [[0,2,7],'sus2 (2)'];
-const sus4 = [[0,5,7],'sus4 (4)'];
-const kwint = [[0,7],'5 (powerchord)'];
-const sep7w = [[0,4,7,11],'dur z septyma wielka (Cmaj7+)'];
-const sep75 = [[0,4,7,11],'Septymowy z kwintą zmniejszoną (C7/5)'];
+//nazewnictwo [[dzwieki],[dur/mol],'nazwa','dopisek notacji']
+
+const xdur      =   [[0,4,7],'dur','Dur',''];
+const xmol      =   [[0,3,7],'mol','Moll',''];
+const xzm       =   [[0,3,6],'mol','zmniejszone','-'];
+const xzw       =   [[0,4,8],'dur','zwiekszone','+'];
+const xdur7     =   [[0,4,7,10],'dur','septymowy','7'];
+const xmol7     =   [[0,3,8,10],'mol','septymowy','7'];
+const xsus2     =   [[0,2,7],'dur','zawieszony sekundowy','sus2'];
+const xsus4     =   [[0,5,7],'dur','zawieszony kwartowy','sus4'];
+const xkwint    =   [[0,7],'','powerchord','5'];
+const xdsep7w   =   [[0,4,7,11],'dur','Durowy z septymą wielką','7+'];
+const xmsep7w   =   [[0,3,7,11],'mol','Molowy z septymą wielką','7+'];
+const xdsep75   =   [[0,4,6,10],'dur','Septymowy z kwintą zmniejszoną','7/5-'];
+const xmsep75   =   [[0,3,6,10],'mol','Septymowy z kwintą zmniejszoną','7/5-'];
+const xzero     =   [[0,3,6,9],'dur','zerowy','0'];
+const xdsext    =   [[0,4,7,9],'dur','Sekstowy','6'];
+const xdsextzm  =   [[0,4,7,8],'dur','Sekstowy z sekstą zmniejszoną','6-'];
+const xdsepkwzw =   [[0,4,8,10],'dur','Septymowy z kwintą zwiększoną','7/5+'];
+const xdnona    =   [[0,4,7,10,14],'dur','Nonowy','9'];
+const xdundec   =   [[0,4,7,10,14,17],'dur','Undecymowy','11'];
+const xseksepm  =   [[0,2,7,10],'dur','sekundowy z septymą małą','7sus2'];
+
 
